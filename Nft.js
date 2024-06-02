a92549c7d70a667a3848a449dd19b8a1fc0d47b3
youmadcute <YOUMADCUTE@protonmail.com>
2024-06-02 22:21:22 +0530

    just an updated

--- Nft.js
+++ Nft.js
@@ -1,9 +1,7 @@
-// Create a variable to hold your NFT's
+
 const nfts = [];
 
-// This function will take in some values as parameters, create an
-// NFT object using the parameters passed to it for its metadata, 
-// and store it in the variable above.
+
 function mintNFT(name, eyeColor, shirtType, bling) {
     const nft = {
         name: name,
@@ -14,8 +12,6 @@ function mintNFT(name, eyeColor, shirtType, bling) {
     nfts.push(nft);
 }
 
-// Create a "loop" that will go through an "array" of NFT's
-// and print their metadata with console.log()
 function listNFTs() {
     for (let i = 0; i < nfts.length; i++) {
         console.log(`NFT #${i + 1}`);
@@ -27,20 +23,19 @@ function listNFTs() {
     }
 }
 
-// Print the total number of NFTs we have minted to the console
+
 function getTotalSupply() {
     return nfts.length;
 }
 
-// Call your functions below this line
 
-// Mint some NFTs
-mintNFT("CryptoPunk", "Blue", "T-Shirt", "Gold Chain");
-mintNFT("BoredApe", "Green", "Hoodie", "Silver Ring");
-mintNFT("Degen", "Brown", "Leather Jacket", "Diamond Watch");
 
-// List all NFTs
+
+mintNFT("CryptoPunk", "Blue-neon", "T-Shirt", "Gold Chain teeth");
+mintNFT("BoredApe", "Green-neon", "half-Hoodie", "Silver Ring-necklace ");
+mintNFT("Degen", "Brown", "Leather Jacket", "Diamond Watch & teeth");
+mintNFT("Cowbelly", "green", "cloth Jacket", "greenn fn");
+
 listNFTs();
 
-// Print the total supply of NFTs
 console.log("Total Supply: " + getTotalSupply());
